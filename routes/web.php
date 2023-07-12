e04eece475f941d005d753ab14befe684049eb9b
Duonglor <quyduong1201@gmail.com>
2023-07-12 08:53:31 +0700

    php3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/student',[StudentController::class,'index'])->name('student');
+Route::get('/student/create',[StudentController::class,'create'])->name('student.create');
+Route::post('/student/store',[StudentController::class,'store'])->name('student.store');
